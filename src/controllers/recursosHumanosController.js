457740af1dd8b349787a1125f87b55614c04acbb
German Archila <106097211+4rchila@users.noreply.github.com>
2025-03-04 22:32:28 -0600

    commit
    cambios si significativos

--- src/controllers/recursosHumanosController.js
+++ src/controllers/recursosHumanosController.js
@@ -1,16 +1,16 @@
-const recursosHumanosController = {};  // Cambiar nombre
+const recursosHumanosController = {};
 
 recursosHumanosController.list = (req, res) => {
     req.getConnection((err, conn) => {
         if (err) {
-            res.json(err);
-            return;
+            console.error(err);
+            return res.status(500).json(err);
         }
 
         conn.query('SELECT * FROM empleados', (err, empleados) => {
             if (err) {
-                res.json(err);
-                return;
+                console.error(err);
+                return res.status(500).json(err);
             }
 
             res.render('empleados', { data: empleados });
@@ -18,4 +18,70 @@ recursosHumanosController.list = (req, res) => {
     });
 };
 
-module.exports = recursosHumanosController;  // Asegurar exportación correcta
+recursosHumanosController.save = (req, res) => {
+    const data = req.body;
+    req.getConnection((err, conn) => {
+        if (err) throw err;
+
+        conn.query('INSERT INTO empleados SET ?', [data], (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Error al guardar el empleado");
+            }
+            res.redirect('/recursosHumanos');
+        });
+    });
+};
+
+recursosHumanosController.edit = (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, conn) => {
+        if (err) throw err;
+
+        conn.query('SELECT * FROM empleados WHERE id = ?', [id], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Error al obtener el empleado");
+            }
+
+            if (result.length === 0) {
+                return res.status(404).send("Empleado no encontrado");
+            }
+
+            res.render('empleados_edit', { empleado: result[0] });
+        });
+    });
+};
+
+recursosHumanosController.delete = (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, conn) => {
+        if (err) throw err;
+
+        conn.query('DELETE FROM empleados WHERE id = ?', [id], (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Error al eliminar el empleado");
+            }
+            res.redirect('/recursosHumanos');
+        });
+    });
+};
+
+recursosHumanosController.update = (req, res) => {
+    const { id } = req.params;
+    const newEmpleado = req.body;
+    req.getConnection((err, conn) => {
+        if (err) throw err;
+
+        conn.query('UPDATE empleados SET ? WHERE id = ?', [newEmpleado, id], (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Error al actualizar el empleado");
+            }
+            res.redirect('/recursosHumanos');
+        });
+    });
+};
+
+module.exports = recursosHumanosController;
--- src/routers/recursosHumanos.js
+++ src/routers/recursosHumanos.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const  recursosHumanosController = require('../controllers/recursosHumanosController')
+const recursosHumanosController = require('../controllers/recursosHumanosController');
 
-router.get('/', recursosHumanosController.list)
+router.get('/', recursosHumanosController.list);
+router.post('/add', recursosHumanosController.save);
+router.get('/delete/:id', recursosHumanosController.delete);
+
+router.get('/update/:id', recursosHumanosController.edit);
+router.post('/update/:id', recursosHumanosController.update);
 
 module.exports = router;
+
